f85a4ddd768c50e5639916f5a16385402a4f8850
Timofey Krivenkov <drake9100@mail.ru>
2023-11-30 10:37:59 +0300

    ADD: Переключение слайдов

--- src/js/components/next.js
+++ src/js/components/next.js
@@ -1,15 +1,51 @@
-function showEl(el, how){
-  el.style.display = how;
-  setTimeout(() => {
-      el.style.opacity = "1";
-  }, 50);
+const sections = document.querySelectorAll('.content-section');
+const nextButton = document.querySelector('[data-step="next"]');
+const prevButtons = document.querySelectorAll('.btn__back');
+
+function switchSection() {
+  let activeSection = 0;
+
+  nextButton.addEventListener('click', (e)=>{
+    go(e.target)
+
+    if(activeSection == sections.length - 1) {
+      nextButton.style.opacity = '0';
+      nextButton.style.pointerEvents = 'none';
+    }
+  }, false)
+
+  prevButtons.forEach(el => {
+    el.addEventListener('click', (e)=>{
+      go(e.target)
+    }, false)
+  })
+
+  function go(el) {
+    if (el.dataset.step === 'next') {
+      activeSection++;
+      activeSection = (activeSection > sections.length - 1) ? 0 : activeSection;
+    }
+
+    if (el.classList.contains('btn__back')) {
+      activeSection--;
+      activeSection = (activeSection < 0) ? sections.length - 1 : activeSection;
+  }
+
+    for (let i = 0; i < sections.length; i++) {
+      sections[i].classList.remove("content-section--active");
+
+      if (i == activeSection){
+        sections[i].className += " content-section--active";
+        let currentType = sections[i].dataset.typeBlock
+
+        document.querySelectorAll(`.pagination__item`).forEach(el => {
+          el.classList.remove('pagination__item--active')
+        })
+        document.querySelector(`[data-pagination-type="${currentType}"]`).classList.add('pagination__item--active')
+      }
+    }
+  }
 }
 
-function hideEl(el){
-  el.style.opacity = "0";
-  setTimeout(() => {
-      el.style.display = "none";
-  }, 300);
-}
-
+switchSection()
 
