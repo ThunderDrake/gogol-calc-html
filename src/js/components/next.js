2373b72aff3ea2ee17f48372575f411fc700e98a
Guihal <guihalbonik@gmail.com>
2023-12-02 17:26:47 +0300

    ref: переключение слайдов

--- src/js/components/next.js
+++ src/js/components/next.js
@@ -2,6 +2,23 @@ const sections = document.querySelectorAll('.content-section');
 const nextButton = document.querySelector('[data-step="next"]');
 const prevButtons = document.querySelectorAll('.btn__back');
 
+sections[0].style.display = "block";
+
+function show(el, how){
+  el.style.display = how;
+  setTimeout(() => {
+    el.style.opacity = 1;
+  }, 50);
+}
+
+
+function hide(el, transition){
+  el.style.opacity = 0;
+  setTimeout(() => {
+    el.style.display = "none";
+  }, transition);
+}
+
 function switchSection() {
   let activeSection = 0;
 
@@ -11,6 +28,9 @@ function switchSection() {
     if(activeSection == sections.length - 1) {
       nextButton.style.opacity = '0';
       nextButton.style.pointerEvents = 'none';
+    } else{
+      nextButton.style.opacity = '1';
+      nextButton.style.pointerEvents = 'auto';
     }
   }, false)
 
@@ -32,16 +52,29 @@ function switchSection() {
   }
 
     for (let i = 0; i < sections.length; i++) {
-      sections[i].classList.remove("content-section--active");
+        sections[i].classList.remove("content-section--active");
+        console.log(sections[0].style.display);
+        hide(sections[i], 200);
+
+
+
 
       if (i == activeSection){
         sections[i].className += " content-section--active";
         let currentType = sections[i].dataset.typeBlock
-
+        setTimeout(() =>{
+          show(sections[i], "block");
+        },250)
         document.querySelectorAll(`.pagination__item`).forEach(el => {
           el.classList.remove('pagination__item--active')
         })
-        document.querySelector(`[data-pagination-type="${currentType}"]`).classList.add('pagination__item--active')
+        if(document.querySelector(`[data-pagination-type="${currentType}"]`)){
+          show(document.querySelector(".pagination__wrapper"), "flex");
+          document.querySelector(`[data-pagination-type="${currentType}"]`).classList.add('pagination__item--active')
+        }else{
+          hide(document.querySelector(".pagination__wrapper"), 100);
+        }
+
       }
     }
   }
