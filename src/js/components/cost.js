0878d4913c09972331931890b74103c3a8662a22
Guihal <guihalbonik@gmail.com>
2023-12-04 05:27:19 +0300

    cost

--- src/js/components/cost.js
+++ src/js/components/cost.js
@@ -1,5 +1,6 @@
 const main = document.querySelector("main");
 const cost = document.querySelector(".cost");
+const costLast = document.querySelector(".cost__last");
 
 
 
@@ -13,34 +14,74 @@ function formatCost(number){
       counter = 0;
       numberTotal = " " +  number[i] + numberTotal;
     }else{
-      numberTotal =  number[i] + numberTotal;
+      if(number[i] == "-"){
+        numberTotal = number[i] + " "  + numberTotal;
+      } else{
+        numberTotal =  number[i] + numberTotal;
+      }
     }
   }
   console.log(numberTotal);
   return numberTotal;
 }
 
-if(main && cost){
+
+function bodyCost() {
+
+}
+
+if(main && cost && costLast){
   let costNum = 0;
-  main.addEventListener('click', (event) => {
-    console.log(event.target.tagName);
-    if(event.target.tagName == "INPUT" || event.target.tagName == "LABEL" || event.target.classList.contains('choices')){
+  let temp = 0;
+  setInterval(() => {
       costNum = 0;
-      const allInputsChecked = document.querySelectorAll("input");
+      const allInputsChecked = document.querySelectorAll("input, select");
       console.log(allInputsChecked.length);
       allInputsChecked.forEach(el => {
         if((el.checked && el.name != "model" || el.selected && el.name != "model") && /\d/.test(el.value)){
-          costNum += Number(el.value);
-          console.log(costNum);
+          if(el.classList.contains("floor__metr")){
+            costNum += Number(el.value * Number(document.querySelector(".floor__input").value));
+          }else if(el.classList.contains("terrace__metr")){
+            costNum += Number(el.value * Number(document.querySelector(".terrace__input").value));
+          }else if(el.classList.contains("lamp__metr")){
+            costNum += Number(el.value * Number(document.querySelector(".lamp__input").value));
+          } else if(el.classList.contains("teplo__metr")) {
+            costNum += Number(el.value * (Number(document.querySelector("[data-choices='insulation']").value) / 50) );
+          } else if(el.classList.contains("teplo-banya__metr")) {
+            costNum += Number(el.value * (Number(document.querySelector("[data-choices='bath_square']").value) / 0.5) );
+          } else if(el.classList.contains("choices__height")) {
+            costNum += Number(1 * (Number(el.value) / 10) );
+          }
+
+          else{
+            costNum += Number(el.value);
+            console.log(costNum);
+          }
+
+
         }
       });
       if(costNum != 0){
         cost.textContent = formatCost(costNum);
+        if(costNum != temp){
+          let lastNum = costNum - temp;
+          if(lastNum > 0){
+            costLast.textContent = "+ " + formatCost(lastNum);
+          } else{
+            costLast.textContent = formatCost(lastNum);
+          }
+
+        }
+        costLast.style.opacity = 1;
         cost.style.opacity = 1;
-        cost.parentNode.style.opacity = 1;
+        cost.parentNode.parentNode.style.opacity = 1;
       } else{
         cost.style.opacity = 0;
       }
-    }
-  });
+
+      temp = costNum;
+  }, 200);
+
+
+
 }
