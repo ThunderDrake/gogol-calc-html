01a861a560d721357193719706b0f6a6f6b8157e
Timofey Krivenkov <drake9100@mail.ru>
2023-12-04 03:52:55 +0300

    ADD: Переключение вкладок по нижним кнопкам

--- src/js/components/next.js
+++ src/js/components/next.js
@@ -1,6 +1,7 @@
 const sections = document.querySelectorAll('.content-section');
 const nextButton = document.querySelector('[data-step="next"]');
 const prevButtons = document.querySelectorAll('.btn__back');
+const bottomButtons = document.querySelectorAll('.pagination__item');
 
 sections[0].style.display = "block";
 
@@ -18,9 +19,9 @@ function hide(el, transition){
     el.style.display = "none";
   }, transition);
 }
+let activeSection = 0;
 
 function switchSection() {
-  let activeSection = 0;
 
   nextButton.addEventListener('click', (e)=>{
     go(e.target)
@@ -78,6 +79,44 @@ function switchSection() {
       }
     }
   }
+}
+
+if(bottomButtons.length > 0) {
+  bottomButtons.forEach(el => {
+    el.addEventListener('click', (e)=>{
+      const target = e.currentTarget;
+      const sectionName = target.dataset.paginationType;
+
+      const sectionId = [...sections].findIndex(el => {
+        return el.dataset.typeBlock === sectionName
+      });
+
+      activeSection = sectionId;
+
+      for (let i = 0; i < sections.length; i++) {
+        sections[i].classList.remove("content-section--active");
+        hide(sections[i], 200);
+
+      if (i == activeSection){
+        sections[i].className += " content-section--active";
+        let currentType = sections[i].dataset.typeBlock
+        setTimeout(() =>{
+          show(sections[i], "block");
+        },250)
+        document.querySelectorAll(`.pagination__item`).forEach(el => {
+          el.classList.remove('pagination__item--active')
+        })
+        if(document.querySelector(`[data-pagination-type="${currentType}"]`)){
+          show(document.querySelector(".pagination__wrapper"), "flex");
+          document.querySelector(`[data-pagination-type="${currentType}"]`).classList.add('pagination__item--active')
+        }else{
+          hide(document.querySelector(".pagination__wrapper"), 100);
+        }
+
+      }
+    }
+    })
+  })
 }
 
 switchSection()
